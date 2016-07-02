8999a2876471377930b8b0e2c0981fb20e139071
Greg Knox <knoxg@randomnoun.com>
2016-07-02 20:58:02 +0000

    suppressEol declaration

--- src/main/java/com/randomnoun/common/jessop/JessopDeclarations.java
+++ src/main/java/com/randomnoun/common/jessop/JessopDeclarations.java
@@ -2,6 +2,9 @@ package com.randomnoun.common.jessop;
 
 /** Things that are defined in the <%@ jessop ... %> declaration.
  * 
+ * <p>Note that the target language isn't currently stored in here; the AbstractJessopScriptBuilder will change
+ * the JessopScriptBuilder implementation instead.
+ * 
  * @author knoxg
  * @version $Id$
  */
@@ -12,8 +15,18 @@ public class JessopDeclarations {
     // pageEncoding="utf-8"
     // suppressEol="true" <-- if a line consists just of a '%>' token, then don't generate the newline after it in the output
 	//   (seeing as I'm mainly using this for code generators at the moment, and I don't like all this whitespace)
+	// NB: still want line numbers to match between jessop source and target language source.
 	
+	public boolean isSuppressEol() {
+		return suppressEol;
+	}
+
+	public void setSuppressEol(boolean suppressEol) {
+		this.suppressEol = suppressEol;
+	}
+
 	String engine;
+	boolean suppressEol;
 	
 	public String getEngine() {
 		return engine;
