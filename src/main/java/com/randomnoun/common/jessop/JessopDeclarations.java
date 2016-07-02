6ef5d1c18b3954dbdeef21e6a093a1289b2ef042
Greg Knox <knoxg@randomnoun.com>
2016-07-02 19:33:54 +0000

    javadoc

--- src/main/java/com/randomnoun/common/jessop/JessopDeclarations.java
+++ src/main/java/com/randomnoun/common/jessop/JessopDeclarations.java
@@ -1,7 +1,18 @@
 package com.randomnoun.common.jessop;
 
+/** Things that are defined in the <%@ jessop ... %> declaration.
+ * 
+ * @author knoxg
+ * @version $Id$
+ */
 public class JessopDeclarations {
 
+	// things we might want to add later:
+	// contentType="text/html; charset=utf-8"
+    // pageEncoding="utf-8"
+    // suppressEol="true" <-- if a line consists just of a '%>' token, then don't generate the newline after it in the output
+	//   (seeing as I'm mainly using this for code generators at the moment, and I don't like all this whitespace)
+	
 	String engine;
 	
 	public String getEngine() {
