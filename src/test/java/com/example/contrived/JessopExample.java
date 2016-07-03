fee130e025ce86019b1f007d25e37051cd3647ed
Greg Knox <knoxg@randomnoun.com>
2016-07-03 21:06:47 +0000

    the example used in index.html.vm

---
+++ src/test/java/com/example/contrived/JessopExample.java
@@ -0,0 +1,30 @@
+package com.example.contrived;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import java.util.Scanner;
+
+/** Simple jessop example  
+ * 
+ * @author knoxg
+ * @version $Id$
+ */
+public class JessopExample {
+
+	public void main(String args[]) throws FileNotFoundException, ScriptException {
+		String filename = args[1];
+		String input = new Scanner(new File(filename)).useDelimiter("\\Z").next();
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("jessop");
+		if (engine==null) { throw new IllegalStateException("Missing engine 'jessop'"); }
+		Bindings b = engine.createBindings();
+		b.put("name", "Baron von Count");
+		b.put("maxCount", 4);
+		engine.eval(input);
+	}
+}
