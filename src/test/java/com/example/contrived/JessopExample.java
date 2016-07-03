99d8adc5adb795ebd219f4e746329a3a47dd8115
Greg Knox <knoxg@randomnoun.com>
2016-07-03 22:42:16 +0000

    comment only

--- src/test/java/com/example/contrived/JessopExample.java
+++ src/test/java/com/example/contrived/JessopExample.java
@@ -22,6 +22,8 @@ public class JessopExample {
 		String input = new Scanner(new File(filename)).useDelimiter("\\Z").next();
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("jessop");
 		if (engine==null) { throw new IllegalStateException("Missing engine 'jessop'"); }
+		// maybe put a function in here, to be called by the script
+		// or a compound object
 		Bindings b = engine.createBindings();
 		b.put("name", "Baron von Count");
 		b.put("maxCount", 4);
